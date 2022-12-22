9a68481d1b093cbec088b528fcb659e47cafddcb
Serge K <cmt@phnx47.net>
2022-12-22 23:38:34 +0700

    fix: don't read slack webhook (#71)

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ if (!process.env.HARVEST_ACCESS_TOKEN)
 if (!process.env.HARVEST_ACCOUNT_ID)
   throw new Error('Environment variable "HARVEST_ACCOUNT_ID" is not defined')
 
-if (!process.env.SLACK_WEBHOOK_URL)
-  throw new Error('Environment variable "SLACK_WEBHOOK_URL" is not defined')
+//if (!process.env.SLACK_WEBHOOK_URL)
+//  throw new Error('Environment variable "SLACK_WEBHOOK_URL" is not defined')
 
 const harvest = new Harvest(process.env.HARVEST_ACCESS_TOKEN, process.env.HARVEST_ACCOUNT_ID);
 //const slack = new Slack(process.env.SLACK_WEBHOOK_URL);
