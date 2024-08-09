2e4b08ea9754b7b829808be3850fa89cb55c69df
Serge K <cmt@phnx47.net>
2024-08-09 20:25:31 +0700

    style: format code

--- cli.js
+++ cli.js
@@ -26,7 +26,7 @@ const firstDay = new Date(today.setDate(today.getDate() - today.getDay() + 1));
 const lastDay = new Date(today.setDate(today.getDate() - today.getDay() + 5));
 
 const from = new Date(firstDay.getTime() - (firstDay.getTimezoneOffset() * 60000)).toISOString().slice(0, 10);
-const to =  new Date(lastDay.getTime() - (lastDay.getTimezoneOffset() * 60000)).toISOString().slice(0, 10);
+const to = new Date(lastDay.getTime() - (lastDay.getTimezoneOffset() * 60000)).toISOString().slice(0, 10);
 
 const harvest = new Harvest();
 const timeEntries = await harvest.getTimeEntries(from, to);
